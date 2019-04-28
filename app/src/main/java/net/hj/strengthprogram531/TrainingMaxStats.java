f79773793c2c7db3b94d306887b078a2dfa04b40
Hreinn Juliusson <hreinn.juliusson@aurorainnovation.com>
2019-04-28 17:12:35 +0200

    My first program

---
+++ app/src/main/java/net/hj/strengthprogram531/TrainingMaxStats.java
@@ -0,0 +1,11 @@
+package net.hj.strengthprogram531;
+
+public class TrainingMaxStats {
+
+    private class TrainingMax{
+        private double squat;
+        private double benchPress;
+        private double deadlift;
+        private double overheadPress;
+    }
+}
